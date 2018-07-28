48f286c3cd8c1399f9ff479d5ebb2e7621db330d
Mike Shepard <mshepard70@gmail.com>
2018-07-28 18:20:37 -0500

    Initial CI commit

---
+++ ContextSensitiveMenus/Examples/wpf_menu_output.ps1
@@ -0,0 +1,19 @@
+﻿ipmo wpf_dsl -force
+ipmo ContextSensitiveMenus -force
+
+$files=get-childitem c:\temp -file | Select-Object -first 5
+
+$w= window2 {textbox Fred 'Hello world'
+            ListBox files $files
+            Checkbox Blah
+            MultiLineTextBox Output -property @{FontFamily='Consolas'}}  
+                
+add-typemenuitem -typename System.IO.FileInfo -label 'Show Contents' -action {Param($file) Get-Content -Path $file.FullName}
+add-typemenuitem -typename System.Object -label 'Show Details' -action {$args[0] | select-object * | out-string -width 200}
+add-typemenuitem -typename System.ServiceProcess.ServiceController -label 'Stop Service' -action {$args[0] | stop-service}
+$txt=$w.GetControlByName('Output')
+add-ContextMenuToControl -window $w -controlName Files -output {
+param([parameter(ValueFromPipeline=$true)][PSObject]$params) $params | foreach {$txt.Text=$txt.Text+$_.Tostring()+"`r`n"}
+}
+
+$w.ShowDialog()
---
+++ psake.ps1
@@ -0,0 +1,108 @@
+# PSake makes variables declared here available in other scriptblocks
+# Init some things
+Properties {
+    # Find the build folder based on build system
+    $ProjectRoot = $ENV:BHProjectPath
+    if (-not $ProjectRoot) {
+        $ProjectRoot = Resolve-Path "$PSScriptRoot\.."
+    }
+    $modulePath=$env:BHModulePath
+    $Timestamp = Get-Date -UFormat "%Y%m%d-%H%M%S"
+    $PSVersion = $PSVersionTable.PSVersion.Major
+    $TestFile = "TestResults_PS$PSVersion`_$TimeStamp.xml"
+    $lines = '----------------------------------------------------------------------'
+
+    $Verbose = @{}
+    if ($ENV:BHCommitMessage -match "!verbose") {
+        $Verbose = @{Verbose = $True}
+    }
+}
+
+Task Default -Depends Deploy
+
+Task Init {
+    $lines
+    Set-Location $ProjectRoot
+    "Build System Details:"
+    Get-Item ENV:BH*
+    "`n"
+    Set-ModuleFunctions -FunctionsToExport '*'
+    Set-ModuleAliases -AliasesToExport '*'
+
+}
+
+Task Docs -Depends Init {
+    if(test-path $ModulePath\Docs){
+      Remove-Item $ModulePath\Docs -Recurse -Force -ErrorAction SilentlyContinue
+    }
+    Import-Module $ENV:BHPSModuleManifest -force -Global -verbose
+    New-MarkdownHelp -Module $env:BHProjectName -OutputFolder $ModulePath\Docs
+}
+
+Task Test -Depends Docs {
+    $lines
+    "`n`tSTATUS: Testing with PowerShell $PSVersion"
+
+    # Testing links on github requires >= tls 1.2
+    $SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+    $TestTags=@{}
+    #only do CI-tagged tests on appveyor
+    If ($ENV:BHBuildSystem -eq 'AppVeyor') {
+        $TestTags=@{ExcludeTag='Local'}
+    }
+    # Gather test results. Store them in a variable and file
+    $TestResults = Invoke-Pester -Path $ProjectRoot\Tests -PassThru -OutputFormat NUnitXml -OutputFile "$ProjectRoot\$TestFile" @TestTags
+    [Net.ServicePointManager]::SecurityProtocol = $SecurityProtocol
+
+    # In Appveyor?  Upload our tests! #Abstract this into a function?
+    If ($ENV:BHBuildSystem -eq 'AppVeyor') {
+        (New-Object 'System.Net.WebClient').UploadFile(
+            "https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)",
+            "$ProjectRoot\$TestFile" )
+    }
+
+    Remove-Item "$ProjectRoot\$TestFile" -Force -ErrorAction SilentlyContinue
+    # Failed tests?
+    # Need to tell psake or it will proceed to the deployment. Danger!
+    if ($TestResults.FailedCount -gt 0) {
+        Write-Error "Failed '$($TestResults.FailedCount)' tests, build failed"
+    }
+    "`n"
+}
+
+Task Build -Depends Test {
+    $lines
+
+    # Load the module, read the exported functions, update the psd1 FunctionsToExport
+    Set-ModuleFunctions
+    Set-ModuleAliases
+
+    # Bump the module version if we didn't already
+    Try {
+        $GalleryVersion = Get-NextNugetPackageVersion  -Name $env:BHProjectName -ErrorAction Stop
+        $GithubVersion = Get-MetaData -Path $env:BHPSModuleManifest -PropertyName ModuleVersion -ErrorAction Stop
+        if ($GalleryVersion -ge $GithubVersion) {
+            Update-Metadata -Path $env:BHPSModuleManifest -PropertyName ModuleVersion -Value $GalleryVersion -ErrorAction stop
+        }
+    } Catch {
+        "Failed to update version for '$env:BHProjectName': $_.`nContinuing with existing version"
+    }
+}
+
+Task Deploy -Depends Build {
+    $lines
+    if (
+        $ENV:BHBuildSystem -ne 'Unknown' -and
+        $ENV:BHBranchName -eq "master" -and
+        $ENV:BHCommitMessage -match '!deploy'
+    ) {
+        $Params = @{
+            Path    = "$ProjectRoot"
+            Force   = $true
+            Recurse = $false # We keep psdeploy artifacts, avoid deploying those : )
+        }
+        Invoke-PSDeploy @Verbose @Params
+    }
+}
